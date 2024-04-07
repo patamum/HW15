9e7d9cff64b2fb8a023f34d2b7a96ec5597a04c1
Svetlana Jourkova <belkina_s@mail.ru>
2024-04-08 00:03:46 +0300

    Добавьте файлы проекта.

---
+++ Task15_1/GeomProgression.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task15_1
+{
+    class GeomProgression : ISeries
+
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int getNext()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void setStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+}
---
+++ Task15_1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task15_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();
+            int start = 5;
+            int step = 3;
+            arith.setStart(start);
+            arith.setStep(step);
+            Console.WriteLine("Арифметическая прогресcия");
+            Console.WriteLine(start);
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    Console.WriteLine("Сброс");
+                    arith.reset();
+                    Console.WriteLine(start);
+                }                    
+                Console.WriteLine(arith.getNext());
+            }
+            
+            GeomProgression geom = new GeomProgression();
+            geom.setStart(start);
+            geom.setStep(step);
+            Console.WriteLine("Геометрическая прогресcия");
+            Console.WriteLine(start);
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    Console.WriteLine("Сброс");
+                    geom.reset();
+                    Console.WriteLine(start);
+                }
+                Console.WriteLine(geom.getNext());
+            }
+
+        }
+    }
+}
